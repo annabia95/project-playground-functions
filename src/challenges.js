9aef822046df60eafc12604092d736f33fa0f624
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-12-02 17:05:53 -0300

    Cumprindo o Oitavo Requisito!

--- src/challenges.js
+++ src/challenges.js
@@ -46,9 +46,22 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+let resultado= [];
+  for (let index = 0; index < numbers.length; index +=1) {
+    if (numbers[index] % 3 === 0 && numbers[index] % 5  === 0){
+    resultado.push ("fizzBuzz");
+    }else if (numbers[index] % 3 === 0) {
+      resultado.push ("fizz");
+    }else if (numbers[index] % 5 === 0) {
+      resultado.push ("buzz");
+    }else {
+      resultado.push ("bug!")
+    }
+  }
+return resultado;
 }
+console.log (fizzBuzz ([2, 15, 7, 9, 45]));
 
 // Desafio 9
 function encode() {
