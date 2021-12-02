9ceca8e7cda4fdaa7f8aa170371b200b9d30ed29
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-12-02 16:20:23 -0300

    Cumprindo o Quinto Requisito!

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,14 @@
 // Desafio 1
-function compareTrue(p1,p2) {
-  if (p1===true && p2===true) {
+function compareTrue(p1, p2) {
+  if (p1 === true && p2 === true) {
     return true;
   } else {
     return false;
   }
 }
 // Desafio 2
-function calcArea(base,height) {
-  let area = (base*height)/2;
+function calcArea(base, height) {
+  let area = (base * height) / 2;
   return area;
 }
 // Desafio 3 - Usei como referência o blog da Trybe para usar o comando split. link:https://blog.betrybe.com/javascript/javascript-split/ 
@@ -22,14 +22,13 @@ function concatName(array) {
   let primeiroNome = array.shift();
   let ultimoNome = array.pop();
   return ultimoNome + ", " + primeiroNome;
-  // seu código aqui
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3) + ties;
+  return pontos;
 }
-
 // Desafio 6
 function highestCount() {
   // seu código aqui
