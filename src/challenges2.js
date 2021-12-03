d84443b963a390b38fdc8a10b6921aa0b4424502
Anna Beatriz Garcia <annagarcia@id.uff.br>
2021-12-02 23:22:17 -0300

    Cumprindo o Requisito 13!

--- src/challenges2.js
+++ src/challenges2.js
@@ -35,12 +35,23 @@ function triangleCheck(lineA, lineB, lineC) {
     return false;
   }
 }
-console.log (triangleCheck (10, 14, 8));
-
-// Desafio 13
-function hydrate() {
-  // seu código aqui
+// Desafio 13 - Usei como referência a documentação do objeto RegExp e do método match(). Links: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/match. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp . https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994 
+function hydrate(stringBebidas) {
+  let sum = 0;
+  let regex = /\d+/g;
+  let num = stringBebidas.match(regex);
+  let arrayNumber = JSON.parse("[" + num + "]");
+  // Agora que já tenho uma array composta pelos números de cada bebida, preciso somá-los!
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    sum += arrayNumber[index];
+  }
+  if (sum > 1) {
+    return sum + ' copos de água';
+  } else {
+    return sum + ' copo de água';
+  }
 }
+console.log(hydrate ('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
